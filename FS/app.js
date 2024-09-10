22f381388b14bd9c0ff4ed74e8420607351310ab
JENISH MENPARA <jenish.m@brilworks.com>
2024-09-10 12:27:50 +0530

    ADD FS
    FS

---
+++ FS/app.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const file = 'input.txt';
+
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    return console.error(`Error reading file: ${err}`);
+  }
+  console.log('Before Update Text:', data);
+
+  fs.writeFile(file, 'Hello, Node i have add new data!', (err) => {
+    if (err) {
+      return console.error(`Error appending file: ${err}`);
+    }
+    fs.readFile(file, 'utf8', (err, updatedData) => {
+      if (err) {
+        return console.error(`Error reading file: ${err}`);
+      }
+      console.log('After appending:', updatedData);
+    });
+  });
+});
